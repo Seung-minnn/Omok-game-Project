61942e21d1df71b3390eb28751141c9c5f0f0d24
하승민 <starriniu@naver.com>
2021-09-08 10:51:19 +0900

    Initaillize project

--- Client/MenuForm.cs
+++ Client/MenuForm.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace Client
 {
-    public partial class Form1 : Form
+    public partial class MenuForm : Form
     {
-        public Form1()
+        public MenuForm()
         {
             InitializeComponent();
         }
+
     }
 }
