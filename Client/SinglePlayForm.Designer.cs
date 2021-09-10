f220af12f0aa1f064e15a67a57dcbeceef3ffe79
하승민 <starriniu@naver.com>
2021-09-10 11:33:37 +0900

    add playbutton ui

--- Client/SinglePlayForm.Designer.cs
+++ Client/SinglePlayForm.Designer.cs
@@ -30,6 +30,8 @@ namespace Client
         private void InitializeComponent()
         {
             this.boardPicture = new System.Windows.Forms.PictureBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.boardPicture)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,11 +46,31 @@ namespace Client
             this.boardPicture.Paint += new System.Windows.Forms.PaintEventHandler(this.boardPicture_Paint);
             this.boardPicture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.boardPicture_MouseDown);
             // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(633, 71);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(100, 40);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "게임 시작";
+            this.playButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(548, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "게임을 시작해주세요.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // SinglePlayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 541);
+            this.ClientSize = new System.Drawing.Size(849, 541);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playButton);
             this.Controls.Add(this.boardPicture);
             this.Name = "SinglePlayForm";
             this.Text = "SinglePlayForm";
@@ -60,5 +82,7 @@ namespace Client
         #endregion
 
         private System.Windows.Forms.PictureBox boardPicture;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label label1;
     }
 }
